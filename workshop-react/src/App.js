018d2ad6618f52ff6a13c09238c9a3b980faa759
Amine1342 <benabdelkader.amine1@gmail.com>
2023-12-01 17:47:06 +0100

    first commit

---
+++ workshop-react/src/App.js
@@ -0,0 +1,23 @@
+import products from "./products.json";
+import Product from "./components/Product";
+import styled from "styled-components";
+import "./App.css";
+
+function App() {
+  return (
+    <AppFrame className="App">
+      {products.map((product, index) => (
+        <Product product={product} key={index}></Product>
+      ))}
+    </AppFrame>
+  );
+}
+
+const AppFrame = styled.div`
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 200px;
+`;
+export default App;
---
+++ workshop-react/src/components/Product.js
@@ -0,0 +1,87 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+
+export default class Product extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { product: props.product, updated: 0 };
+    this.addPrice = this.addPrice.bind(this);
+  }
+
+  componentDidMount() {
+    console.log(
+      "J'ai fini de préparer le produit " +
+        this.props.product.name +
+        " prix : " +
+        this.state.product.price
+    );
+  }
+
+  componentDidUpdate() {
+    console.log("Le prix a été changé " + this.state.updated + " fois");
+  }
+
+  componentWillUnmount() {
+    console.log("On m'a écrasé !");
+  }
+
+  addPrice = (e) => {
+    e.preventDefault();
+    return this.setState((oldState) => ({
+      product: {
+        ...oldState.product,
+        price: Number(oldState.product.price) + 0.1,
+      },
+      updated: oldState.updated + 1,
+    }));
+  };
+  render() {
+    return (
+      <ProductFrame>
+        <ProductImageWrapper>
+          <ProductImage src={this.props.product.img}></ProductImage>
+        </ProductImageWrapper>
+        <ProductInfoWrapper>
+          <span>{this.props.product.name}</span>
+          <span>{this.props.product.price} </span>
+          {Number(this.state.product.price) > 2 ? "Yesir ghali :p" : "Ichri"}
+          <button onClick={this.addPrice}>Ajouter 0.1 TND</button>
+        </ProductInfoWrapper>
+      </ProductFrame>
+    );
+  }
+}
+
+const ProductFrame = styled.div`
+  border-radius: 25px;
+  min-height: 150px;
+  min-width: 150px;
+  background-color: rgb(110, 110, 110, 0.7);
+  margin: 10px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const ProductImage = styled.img`
+  width: 100px;
+  height: 110px;
+  border-radius: 25px;
+  margin-left: 25px;
+  margin-top: 5px;
+`;
+
+const ProductImageWrapper = styled.div`
+  width: 100px;
+  height: 120px;
+  border-radius: 25px;
+`;
+
+const ProductInfoWrapper = styled.div`
+  margin-top: auto;
+  margin-bottom: 5px;
+  display: flex;
+  flex-direction: column;
+  & > span {
+    text-align: center;
+  }
+`;
